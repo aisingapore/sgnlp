913051e92942d69b9f25d5079e9075eaa89a8c7b
Jonathan Heng <jonheng@users.noreply.github.com>
2021-08-04 17:56:57 +0800

    Update setup.py to use README.md as description

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup, find_packages
+from os import path
+
+dir = path.abspath(path.dirname(__file__))
+with open(path.join(dir, 'README.md')) as f:
+    long_description = f.read()
 
 setup(
     name="sgnlp",
     packages=find_packages(),
     version="0.0.1",
-    description="State-of-the-art models from Singapore's NLP research community",
+    description="Machine learning models from Singapore's NLP research community",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author="Jonathan Heng, Raymond Ng, Zhi Chong Tan, Benedict Lee",
     author_email="sg-nlp@aisingapore.org",
     keywords=["NLP", "machine learning", "deep learning"],
