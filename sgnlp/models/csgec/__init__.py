0315956d98a1f0e56a35eadd0473a963efa5effa
Jonathan Heng <jonheng@users.noreply.github.com>
2021-10-08 18:22:59 +0800

    [#32] Add preprocessor, postprocessor, and usage example

--- sgnlp/models/csgec/__init__.py
+++ sgnlp/models/csgec/__init__.py
@@ -1,6 +1,8 @@
 from .config import CSGConfig
 from .tokenization import CSGTokenizer
 from .modeling import CSGModel
+from .preprocess import CsgecPreprocessor
+from .postprocess import CsgecPostprocessor
 from .utils import download_tokenizer_files
 
 import nltk
