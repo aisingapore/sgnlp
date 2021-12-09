5292df778f9a67ad2e860ca37b5f957e629e4013
Raymond Ng <raymond.bcng@gmail.com>
2021-12-09 17:17:17 +0800

    [#41] add gcn class

---
+++ sgnlp/models/asgcn/modules/gcn.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+
+class GraphConvolution(nn.Module):
+    """
+    Simple GCN Layer, similar to https://arxiv.org/abs/1609.02907
+    """
+    def __init__(self, in_features, out_features, bias=True) -> None:
+        super(GraphConvolution, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = nn.Parameter(torch.FloatTensor(in_features, out_features))
+        if bias:
+            self.bias = nn.Parameter(torch.FloatTensor(out_features))
+        else:
+            self.register_parameter('bias', None)
+
+    def forward(self, text, adj):
+        hidden = torch.matmul(text, self.weight)
+        denom = torch.sum(adj, dim=2, keepdim=True) + 1
+        output = torch.matmul(adj, hidden) / denom
+        return output + self.bias if self.bias is not None else output
