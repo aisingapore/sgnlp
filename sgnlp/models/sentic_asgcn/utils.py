0333babcbbcfdd406a83157e96bc1780ff48afc5
Raymond Ng <raymond.bcng@gmail.com>
2021-12-10 13:49:04 +0800

    [#41] add set random seed helper

--- sgnlp/models/sentic_asgcn/utils.py
+++ sgnlp/models/sentic_asgcn/utils.py
@@ -1,6 +1,10 @@
 import argparse
 import json
+import random
 import pathlib
+
+import numpy as np
+import torch
 
 from .data_class import SenticASGCNTrainArgs
 
@@ -23,3 +27,17 @@ def parse_args_and_load_config(
 
     sentic_asgcn_args = SenticASGCNTrainArgs(**cfg)
     return sentic_asgcn_args
+
+
+def set_random_seed(seed: int = 776) -> None:
+    """Helper method to set random seeds for python, numpy and torch
+
+    Args:
+        seed (int, optional): seed value to set. Defaults to 776.
+    """
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
