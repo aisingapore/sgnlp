f8118baf177681d9d53d7aac782a2fa31e1e0822
Raymond Ng <raymond.bcng@gmail.com>
2021-12-29 09:51:52 +0800

    [#41] remove unused attributes

--- sgnlp/models/senticnet_gcn/modules/gcn.py
+++ sgnlp/models/senticnet_gcn/modules/gcn.py
@@ -7,10 +7,8 @@ class GraphConvolution(nn.Module):
     Simple GCN Layer, similar to https://arxiv.org/abs/1609.02907
     """
 
-    def __init__(self, in_features, out_features, bias=True) -> None:
+    def __init__(self, in_features: torch.tensor, out_features: torch.tensor, bias=True) -> None:
         super(GraphConvolution, self).__init__()
-        self.in_features = in_features
-        self.out_features = out_features
         self.weight = nn.Parameter(torch.FloatTensor(in_features, out_features))
         if bias:
             self.bias = nn.Parameter(torch.FloatTensor(out_features))
