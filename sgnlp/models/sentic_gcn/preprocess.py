8f5431acbb444523ef122831c245e9951ae2316d
Raymond Ng <raymond.bcng@gmail.com>
2022-01-07 22:10:24 +0800

    [#41] complete both preprocessor init method

--- sgnlp/models/sentic_gcn/preprocess.py
+++ sgnlp/models/sentic_gcn/preprocess.py
@@ -1,4 +1,5 @@
-from typing import List
+import pathlib
+from typing import Dict, List
 
 import torch
 from transformers import PreTrainedTokenizer
@@ -17,25 +18,42 @@ class SenticGCNPreprocessor:
         embedding_model: PreTrainedModel = None,
         tokenizer_name: str = None,
         embedding_model_name: str = None,
-        device: torch.device = torch.device("cpu"),
+        device: str = "cpu",
     ):
-        self.device = device
+        # Set device
+        self.device = (
+            torch.device("cuda" if torch.cuda.is_available() else "cpu") if not device else torch.device(device)
+        )
+        # Init Tokenizer
         if tokenizer is not None:
             self.tokenizer = tokenizer
         else:
             self.tokenizer = SenticGCNTokenizer.from_pretrained(tokenizer_name)
-
+        # Init Embedding model
         if embedding_model is not None:
             self.embedding_model = embedding_model
+            self.embedding_model.to(self.device)
         else:
-            embedding_config = SenticGCNEmbeddingConfig.from_pretrained(embedding_model_name)
-            self.embedding_model = SenticGCNEmbeddingModel.from_pretrained(
-                embedding_model_name, config=embedding_config
-            ).to(device)
+            embed_model_name = pathlib.Path(embedding_model_name)
+            if embed_model_name.is_dir():
+                config_path = embed_model_name.joinpath("config.json")
+                model_path = embed_model_name.joinpath("pytorch_model.bin")
+                if config_path.exists() and model_path.exists():
+                    embed_config = SenticGCNEmbeddingConfig.from_pretrained(config_path)
+                    embed_model = SenticGCNEmbeddingModel.from_pretrained(model_path, config=embed_config)
+                else:
+                    raise ValueError(
+                        f"""Error creating embedding model! config.json and pytorch_model.bin
+                                        not found in directory {embedding_model_name}."""
+                    )
+            else:
+                embed_config = SenticGCNEmbeddingConfig.from_pretrained(embedding_model_name)
+                embed_model = SenticGCNEmbeddingModel.from_pretrained(embedding_model_name, config=embed_config)
+            self.embedding_model = embed_model
+            self.embedding_model.to(self.device)
 
-    def __call__(self, data_batch: List[str]) -> BatchEncoding:
-        tokens = self.tokenizer(data_batch, padding=True, return_tensors="pt")
-        return tokens
+    def __call__(self, data_batch: List[Dict[str, List[str]]]) -> BatchEncoding:
+        pass  # TODO
 
 
 class SenticGCNBertPreprocessor:
@@ -43,24 +61,40 @@ class SenticGCNBertPreprocessor:
         self,
         tokenizer: PreTrainedTokenizer = None,
         embedding_model: PreTrainedModel = None,
-        tokenizer_name: str = None,
-        embedding_model_name: str = None,
-        device: torch.device = torch.device("cpu"),
+        tokenizer_name: str = "bert-base-uncased",
+        embedding_model_name: str = "bert-base-uncased",
+        device: str = "cpu",
     ):
-        self.device = device
+        # Set device
+        self.device = (
+            torch.device("cuda" if torch.cuda.is_available() else "cpu") if not device else torch.device(device)
+        )
+        # Init Tokenizer
         if tokenizer is not None:
             self.tokenizer = tokenizer
         else:
             self.tokenizer = SenticGCNBertTokenizer.from_pretrained(tokenizer_name)
-
+        # Init Embedding model
         if embedding_model is not None:
             self.embedding_model = embedding_model
         else:
-            embedding_config = SenticGCNBertEmbeddingConfig.from_pretrained(embedding_model_name)
-            self.embedding_model = SenticGCNBertEmbeddingModel.from_pretrained(
-                embedding_model_name, config=embedding_config
-            ).to(device)
+            embed_model_name = pathlib.Path(embedding_model_name)
+            if embed_model_name.is_dir():
+                config_path = embed_model_name.joinpath("config.json")
+                model_path = embed_model_name.joinpath("pytorch_model.bin")
+                if config_path.exists() and model_path.exists():
+                    embed_config = SenticGCNBertEmbeddingConfig.from_pretrained(config_path)
+                    embed_model = SenticGCNBertEmbeddingModel.from_pretrained(model_path, config=embed_config)
+                else:
+                    raise ValueError(
+                        f"""Error creating embedding model! config.json and pytorch_model.bin
+                                        not found in directory {embedding_model_name}."""
+                    )
+            else:
+                embed_config = SenticGCNBertEmbeddingConfig.from_pretrained(embedding_model_name)
+                embed_model = SenticGCNBertEmbeddingModel.from_pretrained(embedding_model_name, config=embed_config)
+            self.embedding_model = embed_model
+            self.embedding_model.to(self.device)
 
-    def __call__(self, data_batch: List[str]) -> BatchEncoding:
-        tokens = self.tokenizer(data_batch, padding=True, return_tensors="pt")
-        return tokens
+    def __call__(self, data_batch: List[Dict[str, List[str]]]) -> BatchEncoding:
+        pass  # TODO
