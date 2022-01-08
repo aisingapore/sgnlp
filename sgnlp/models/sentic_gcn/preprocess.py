f59444acaa5cef933c0d83e35225adf496b3149d
Raymond Ng <raymond.bcng@gmail.com>
2022-01-08 09:22:27 +0800

    [#41] add support to load embedding model from cloud storage

--- sgnlp/models/sentic_gcn/preprocess.py
+++ sgnlp/models/sentic_gcn/preprocess.py
@@ -1,4 +1,5 @@
 import pathlib
+import urllib.parse
 from typing import Dict, List
 
 import torch
@@ -16,8 +17,10 @@ class SenticGCNPreprocessor:
         self,
         tokenizer: PreTrainedTokenizer = None,
         embedding_model: PreTrainedModel = None,
-        tokenizer_name: str = None,
-        embedding_model_name: str = None,
+        tokenizer_name: str = "https://storage.googleapis.com/sgnlp/models/sentic_gcn/senticgcn_tokenizer/",
+        embedding_model_name: str = "https://storage.googleapis.com/sgnlp/models/sentic_gcn/senticgcn_embedding_model/",
+        config_filename: str = "config.json",
+        model_filename: str = "pytorch_model.bin",
         device: str = "cpu",
     ):
         # Set device
@@ -31,24 +34,28 @@ class SenticGCNPreprocessor:
             self.tokenizer = SenticGCNTokenizer.from_pretrained(tokenizer_name)
         # Init Embedding model
         if embedding_model is not None:
+            # Load from external instance
             self.embedding_model = embedding_model
             self.embedding_model.to(self.device)
         else:
-            embed_model_name = pathlib.Path(embedding_model_name)
-            if embed_model_name.is_dir():
-                config_path = embed_model_name.joinpath("config.json")
-                model_path = embed_model_name.joinpath("pytorch_model.bin")
-                if config_path.exists() and model_path.exists():
+            if embedding_model_name.startswith("https://") or embedding_model_name.startswith("http://"):
+                # Load from cloud
+                config_url = urllib.parse.urljoin(embedding_model_name, config_filename)
+                model_url = urllib.parse.urljoin(embedding_model_name, model_filename)
+                embed_config = SenticGCNEmbeddingConfig.from_pretrained(config_url)
+                embed_model = SenticGCNEmbeddingModel.from_pretrained(model_url, config=embed_config)
+            else:
+                # Load from local folder
+                embed_model_name = pathlib.Path(embedding_model_name)
+                if embed_model_name.is_dir():
+                    config_path = embed_model_name.joinpath("config.json")
+                    model_path = embed_model_name.joinpath("pytorch_model.bin")
                     embed_config = SenticGCNEmbeddingConfig.from_pretrained(config_path)
                     embed_model = SenticGCNEmbeddingModel.from_pretrained(model_path, config=embed_config)
                 else:
-                    raise ValueError(
-                        f"""Error creating embedding model! config.json and pytorch_model.bin
-                                        not found in directory {embedding_model_name}."""
-                    )
-            else:
-                embed_config = SenticGCNEmbeddingConfig.from_pretrained(embedding_model_name)
-                embed_model = SenticGCNEmbeddingModel.from_pretrained(embedding_model_name, config=embed_config)
+                    # Load from HuggingFace model repository
+                    embed_config = SenticGCNEmbeddingConfig.from_pretrained(embedding_model_name)
+                    embed_model = SenticGCNEmbeddingModel.from_pretrained(embedding_model_name, config=embed_config)
             self.embedding_model = embed_model
             self.embedding_model.to(self.device)
 
@@ -63,6 +70,8 @@ class SenticGCNBertPreprocessor:
         embedding_model: PreTrainedModel = None,
         tokenizer_name: str = "bert-base-uncased",
         embedding_model_name: str = "bert-base-uncased",
+        config_filename: str = "config.json",
+        model_filename: str = "pytorch_model.bin",
         device: str = "cpu",
     ):
         # Set device
@@ -76,23 +85,30 @@ class SenticGCNBertPreprocessor:
             self.tokenizer = SenticGCNBertTokenizer.from_pretrained(tokenizer_name)
         # Init Embedding model
         if embedding_model is not None:
+            # Load from external instance
             self.embedding_model = embedding_model
+            self.embedding_model.to(self.device)
         else:
-            embed_model_name = pathlib.Path(embedding_model_name)
-            if embed_model_name.is_dir():
-                config_path = embed_model_name.joinpath("config.json")
-                model_path = embed_model_name.joinpath("pytorch_model.bin")
-                if config_path.exists() and model_path.exists():
+            if embedding_model_name.startswith("https://") or embedding_model_name.startswith("http://"):
+                # Load from cloud
+                config_url = urllib.parse.urljoin(embedding_model_name, config_filename)
+                model_url = urllib.parse.urljoin(embedding_model_name, model_filename)
+                embed_config = SenticGCNBertEmbeddingConfig.from_pretrained(config_url)
+                embed_model = SenticGCNBertEmbeddingModel.from_pretrained(model_url, config=embed_config)
+            else:
+                # Load from local folder
+                embed_model_name = pathlib.Path(embedding_model_name)
+                if embed_model_name.is_dir():
+                    config_path = embed_model_name.joinpath("config.json")
+                    model_path = embed_model_name.joinpath("pytorch_model.bin")
                     embed_config = SenticGCNBertEmbeddingConfig.from_pretrained(config_path)
                     embed_model = SenticGCNBertEmbeddingModel.from_pretrained(model_path, config=embed_config)
                 else:
-                    raise ValueError(
-                        f"""Error creating embedding model! config.json and pytorch_model.bin
-                                        not found in directory {embedding_model_name}."""
+                    # Load from HuggingFace model repository
+                    embed_config = SenticGCNBertEmbeddingConfig.from_pretrained(embedding_model_name)
+                    embed_model = SenticGCNBertEmbeddingModel.from_pretrained(
+                        embedding_model_name, config=embed_config
                     )
-            else:
-                embed_config = SenticGCNBertEmbeddingConfig.from_pretrained(embedding_model_name)
-                embed_model = SenticGCNBertEmbeddingModel.from_pretrained(embedding_model_name, config=embed_config)
             self.embedding_model = embed_model
             self.embedding_model.to(self.device)
 
