dfb7f0807db0a5ba82094a08e922abbbcecd4d05
K-WeiMing <weiming.kng@gmail.com>
2022-01-11 18:29:37 +0800

    [#43] Update api.py with senticnet preprocessor from gcloud

--- demo_api/sentic_gcn/api.py
+++ demo_api/sentic_gcn/api.py
@@ -1,50 +1,57 @@
-from flask import request
-from flask import request
 from transformers import cached_path
+
+import torch.nn.functional as F
+
+from flask import request
 
 from demo_api.common import create_api
 from sgnlp.models.sentic_gcn import (
-    SenticGCNModel, 
-    SenticGCNModelOutput, 
-    SenticGCNPreTrainedModel,
-    SenticGCNBertModelOutput,
-    SenticGCNBertPreTrainedModel,
     SenticGCNBertModel,
-    SenticGCNEmbeddingPreTrainedModel,
-    SenticGCNEmbeddingModel,
-    SenticGCNBertEmbeddingModel,
-    SenticGCNConfig,
-    SenticGCNPreprocessor,
-    SenticGCNTokenizer,
-    SenticGCNBertTokenizer
+    SenticGCNBertConfig,
+    SenticGCNBertTokenizer,
+    SenticGCNBertPreprocessor
     )
+
+from sgnlp.models.sentic_gcn.postprocess import SenticGCNBertPostprocessor
+
+from flask import request
+import os
+
 
 app = create_api(app_name=__name__, model_card_path="model_card/sentic_gcn.json")
 
-# Download files from azure blob storage
-#rel2id_path = cached_path("https://storage.googleapis.com/sgnlp/models/lsr/rel2id.json")
-
+preprocessor = SenticGCNBertPreprocessor(senticnet='https://storage.googleapis.com/sgnlp/models/sentic_gcn/senticnet.pickle', device='cpu')
 
 # Load model
+config = SenticGCNBertConfig.from_pretrained("https://storage.googleapis.com/sgnlp/models/sentic_gcn/senticgcn_bert/config.json") # Input JSON file
 
-config = SenticGCNConfig.from_pretrained("") # Input JSON file
-
-model = SenticGCNModel.from_pretrained(
-    "", # Input model
+model = SenticGCNBertModel.from_pretrained(
+    "https://storage.googleapis.com/sgnlp/models/sentic_gcn/senticgcn_bert/pytorch_model.bin",
     config=config
 )
-
-model.eval()
 
 app.logger.info("Preprocessing pipeline and model initialization complete.")
 
 @app.route("/predict", methods=["POST"])
 def predict():
     req_body = request.get_json()
-    document = req_body["document"]
+    # aspect = req_body["aspect"]
+    # sentence = req_body["sentence"]
     
     # Perform preprocessing from the imported pipeline
+    processed_inputs, processed_indices = preprocessor(req_body)
+    outputs = model(processed_indices)
+    t_probs = F.softmax(outputs.logits)
+    t_probs = t_probs.detach().numpy()
+
+    infer_label = [t_probs.argmax(axis=-1)[idx] -1 for idx in range(len(t_probs))]
+    
+    # Postprocessing
+    postprocessor = SenticGCNBertPostprocessor()
+    post_outputs = postprocessor(processed_inputs=processed_inputs, model_outputs=outputs)
+    return post_outputs
     
 
 if __name__ == "__main__":
-    app.run()
+    # app.run()
+    app.run(host="0.0.0.0", debug=True, port=8000)
