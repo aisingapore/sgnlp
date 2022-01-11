3429ce74e5a4c89fd68bbce8284ae4b5484b9b6e
K-WeiMing <weiming.kng@gmail.com>
2022-01-12 02:07:41 +0800

    [#43] Update api.py to a working version with int conversion for output key: labels (commented out)

--- demo_api/sentic_gcn/api.py
+++ demo_api/sentic_gcn/api.py
@@ -1,4 +1,5 @@
 import re
+import json
 from transformers import cached_path
 
 import torch.nn.functional as F
@@ -18,11 +19,12 @@ from sgnlp.models.sentic_gcn.postprocess import SenticGCNBertPostprocessor
 from flask import request
 import os
 
-
 app = create_api(app_name=__name__, model_card_path="model_card/sentic_gcn.json")
 
 # path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static', 'senticnet5.pickle')
 preprocessor = SenticGCNBertPreprocessor(senticnet='https://storage.googleapis.com/sgnlp/models/sentic_gcn/senticnet.pickle', device='cpu')
+
+postprocessor = SenticGCNBertPostprocessor()
 
 # Load model
 config = SenticGCNBertConfig.from_pretrained("https://storage.googleapis.com/sgnlp/models/sentic_gcn/senticgcn_bert/config.json") # Input JSON file
@@ -37,30 +39,23 @@ app.logger.info("Preprocessing pipeline and model initialization complete.")
 @app.route("/predict", methods=["POST"])
 def predict():
     req_body = request.get_json()
-    print(req_body)
-    aspect = req_body["aspect"]
-    sentence = req_body["sentence"]
-    
-    print('aspect: ',aspect)
-    print('sentence: ',sentence)
     
     inputs = list()
     inputs.append(req_body)
+    print(inputs)
     
     # Perform preprocessing from the imported pipeline
     processed_inputs, processed_indices = preprocessor(inputs)
     outputs = model(processed_indices)
     t_probs = F.softmax(outputs.logits)
     t_probs = t_probs.detach().numpy()
-
-    infer_label = [t_probs.argmax(axis=-1)[idx] -1 for idx in range(len(t_probs))]
     
     # Postprocessing
-    postprocessor = SenticGCNBertPostprocessor()
     post_outputs = postprocessor(processed_inputs=processed_inputs, model_outputs=outputs)
     
-    print('post_outputs: ',post_outputs)
-    return jsonify(post_outputs) # to fix the output
+    # Convert labels into int, currently it is a numpy.int which has issues with json
+    # post_outputs[0]['labels'] = [int(x) for x in post_outputs[0]['labels']]
+    return post_outputs[0]
     
 
 if __name__ == "__main__":
