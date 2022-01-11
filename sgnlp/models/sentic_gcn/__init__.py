5f44f6bd7acd86fd451b5aa9ca2c8931593ea6e0
Raymond Ng <raymond.bcng@gmail.com>
2022-01-11 08:49:22 +0800

    [#41] add postprocess and data_class imports

--- sgnlp/models/sentic_gcn/__init__.py
+++ sgnlp/models/sentic_gcn/__init__.py
@@ -1,6 +1,8 @@
 from config import SenticGCNConfig, SenticGCNBertConfig, SenticGCNEmbeddingConfig, SenticGCNBertEmbeddingConfig
+from data_class import SenticGCNTrainArgs
 from modeling import SenticGCNModel, SenticGCNBertModel, SenticGCNEmbeddingModel, SenticGCNBertEmbeddingModel
 from preprocess import SenticGCNPreprocessor, SenticGCNBertPreprocessor
+from postprocess import SenticGCNPostprocessor, SenticGCNBertPostprocessor
 from tokenization import SenticGCNTokenizer, SenticGCNBertTokenizer
 from train import SenticGCNTrainer, SenticGCNBertTrainer
 from utils import BucketIterator, parse_args_and_load_config, download_tokenizer_files
