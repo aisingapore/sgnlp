5f9f816b5577b1542d0ad6c30c7f69f290bd30ef
K-WeiMing <weiming.kng@gmail.com>
2022-01-12 11:58:35 +0800

    [#43] Add preprocessor in download_pretrained.py

--- demo_api/sentic_gcn/download_pretrained.py
+++ demo_api/sentic_gcn/download_pretrained.py
@@ -7,13 +7,15 @@ from sgnlp.models.sentic_gcn import (
     SenticGCNBertPreprocessor
 )
 
+# Downloads preprocessor, pretrained config, tokenizer, model
+preprocessor = SenticGCNBertPreprocessor(
+    senticnet='https://storage.googleapis.com/sgnlp/models/sentic_gcn/senticnet.pickle', 
+    device='cpu'
+)
+tokenizer = SenticGCNBertTokenizer.from_pretrained("bert-base-uncased")
 config = SenticGCNConfig.from_pretrained(
     "https://storage.googleapis.com/sgnlp/models/sentic_gcn/senticgcn_bert/config.json"
 )
-
-tokenizer = SenticGCNBertTokenizer.from_pretrained("bert-base-uncased")
-
-
 model = SenticGCNBertModel.from_pretrained(
     "https://storage.googleapis.com/sgnlp/models/sentic_gcn/senticgcn_bert/pytorch_model.bin", 
     config=config
