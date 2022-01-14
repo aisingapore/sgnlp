1a726243d2af78658539c0bd50eb03138e1ba794
Raymond Ng <raymond.bcng@gmail.com>
2022-01-14 10:31:20 +0800

    [#41] fix wrong imports for __init__

--- sgnlp/models/sentic_gcn/__init__.py
+++ sgnlp/models/sentic_gcn/__init__.py
@@ -1,6 +1,6 @@
 from .config import SenticGCNConfig, SenticGCNBertConfig, SenticGCNEmbeddingConfig, SenticGCNBertEmbeddingConfig
 from .data_class import SenticGCNTrainArgs
-from .eval import SenticGCNEvaluator, SenticGCNBaseEvaluator
+from .eval import SenticGCNEvaluator, SenticGCNBertEvaluator
 from .modeling import SenticGCNModel, SenticGCNBertModel, SenticGCNEmbeddingModel, SenticGCNBertEmbeddingModel
 from .preprocess import SenticGCNPreprocessor, SenticGCNBertPreprocessor
 from .postprocess import SenticGCNPostprocessor, SenticGCNBertPostprocessor
