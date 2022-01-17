b123236c19f6d6e6b817329c960646dd54eb0fd2
Raymond Ng <raymond.bcng@gmail.com>
2022-01-17 21:26:56 +0800

    [#41] update usage with latest examples

--- demo_api/sentic_gcn/usage.py
+++ demo_api/sentic_gcn/usage.py
@@ -1,39 +1,39 @@
 from sgnlp.models.sentic_gcn import (
-    SenticGCNBertModel, 
-    SenticGCNBertPreprocessor, 
+    SenticGCNBertModel,
+    SenticGCNBertPreprocessor,
     SenticGCNBertConfig,
-    SenticGCNBertPostprocessor
-    )
+    SenticGCNBertPostprocessor,
+)
 
 preprocessor = SenticGCNBertPreprocessor(
-    senticnet='https://storage.googleapis.com/sgnlp/models/sentic_gcn/senticnet.pickle', 
-    device='cpu'
+    senticnet="https://storage.googleapis.com/sgnlp/models/sentic_gcn/senticnet.pickle", device="cpu"
 )
 
 postprocessor = SenticGCNBertPostprocessor()
 
 # Load model
-config = SenticGCNBertConfig.from_pretrained('https://storage.googleapis.com/sgnlp/models/sentic_gcn/senticgcn_bert/config.json')
+config = SenticGCNBertConfig.from_pretrained(
+    "https://storage.googleapis.com/sgnlp/models/sentic_gcn/senticgcn_bert/config.json"
+)
 
 model = SenticGCNBertModel.from_pretrained(
-    'https://storage.googleapis.com/sgnlp/models/sentic_gcn/senticgcn_bert/pytorch_model.bin', 
-    config=config
+    "https://storage.googleapis.com/sgnlp/models/sentic_gcn/senticgcn_bert/pytorch_model.bin", config=config
 )
 
 # Inputs
 inputs = [
     {
-        "aspect": ["Soup"],
-        "sentence": "Soup is tasty but soup is a little salty. Salty soup."
-    }, # 1, -1
+        "aspects": ["service", "decor"],
+        "sentence": "Everything is always cooked to perfection , the service is excellent, the decor cool and understated.",
+    },
     {
-        "aspect": ["service"],
-        "sentence": "Everyone that sat in the back outside agreed that it was the worst service we had ever received."
-    }, # -1
+        "aspects": ["food", "portions"],
+        "sentence": "The food was lousy - too sweet or too salty and the portions tiny.",
+    },
     {
-        "aspect": ["location", "food"],
-        "sentence": "it 's located in a strip mall near the beverly center , not the greatest location , but the food keeps me coming back for more ."
-    } # 0, 1
+        "aspects": ["service"],
+        "sentence": "To sum it up : service varies from good to mediorce , depending on which waiter you get ; generally it is just average ok .",
+    },
 ]
 
 processed_inputs, processed_indices = preprocessor(inputs)
